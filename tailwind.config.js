2492454dd809209be7445a08521c8132eff1d84a
professormood <sdas10560@gmail.com>
2023-12-13 00:08:33 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    colors: {
+      Pink: "hsl(322, 100%, 66%)",
+      VeryPaleCyan: "hsl(193, 100%, 96%)",
+      VeryDarkCyan: "hsl(192, 100%, 9%)",
+      GrayishBlue: "hsl(208, 11%, 55%)",
+    },
+    extend: {
+     
+    },
+  },
+  plugins: [],
+};
